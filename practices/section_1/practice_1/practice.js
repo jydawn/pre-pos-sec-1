a30d1cd3b6db01fafdccf32cc5324ed1d6453804
jydawn <1198621249@qq.com>
2016-06-14 18:07:09 +0800

    Update practice.js

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,13 +1,17 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
-  var temp=[];
+  var temp1=[],temp2=[],result=[];
   for(var i in collection_a)
+    temp1.push(collection_a[i]);
   for(var j in collection_b)
-  {
-    if(collection_a[i]==collecton_b[j])
-      temp.push(collection_a[i]);
-  }
-  return temp;
+    temp2.push(collection_b[j]);
+    for(var k=0;k<temp1.length;k++)
+      for(var t=0;t<temp2.length;t++){
+          if(temp1[i]==temp2[j])
+              result.push(temp1[i]);
+        
+      }
+  return result;
 }
 
 module.exports = collect_same_elements;
